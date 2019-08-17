d428bdf5ee007148e9cca9989199d5e29098f8b6
Rochelle Reyes <rochelle.reyes@uabc.edu.mx>
2019-08-17 14:23:03 -0700

    Introduction to Unity

---
+++ DDI-First/Assets/CollisionDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector: MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Juego iniciado");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        /* Debug.Log("Game ongoing"); */
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log("Collision detected with " + collision.gameObject.name);
+
+    }
+
+}

3759c8171b260c4fbaf6b6fe1f384259757b7116
Rochelle Reyes <rochelle.reyes@uabc.edu.mx>
2019-08-30 18:14:02 -0700

    Adding functionality for buttons Continue, Mute/Unmute, Reload Scene

--- DDI-Prac1/Assets/Scripts/Pauser.cs
+++ DDI-Prac1/Assets/Scripts/Pauser.cs
@@ -1,11 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Pauser: MonoBehaviour {
 	private bool paused = false;
 	public GameObject pauseMenu;
 
-	// Update is called once per frame
+	public void ReloadScene() {
+		Scene scene = SceneManager.GetActiveScene();
+		SceneManager.LoadScene(scene.name);
+	}
+
+	public void MuteUnmute() {
+		AudioListener.pause = !AudioListener.pause;
+	}
+
+
+	// Update is called once per frame hola como estas>?
 	void Update() {
 		if (Input.GetKeyUp(KeyCode.P)) {
 			paused = !paused;
